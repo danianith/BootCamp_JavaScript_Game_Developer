0b93801b6597fb838cd8c3b460ada9ff86f6a6f5
danianith <danianith@gmail.com>
2020-09-25 18:47:10 -0300

    Add projeto_jogo_dino

--- projeto_jogo_dino/readme.md
+++ projeto_jogo_dino/readme.md
@@ -1,11 +1,9 @@
 
 Projeto para recriar o jogo do dinossauro sem internet do navegador Chrome. Utilizando HTML5, CSS3 e JavaScript da DIO.
 
-
 <a href="">Acesse meu jogo aqui</a>
 
-
-Recriando o famoso jogo do dinossauro sem internet - Professor: Celso Henrique - DIO.
+* Recriando o famoso jogo do dinossauro sem internet - Professor: Celso Henrique - DIO.
 
 Neste projeto você terá o desafio de criar uma cópia do jogo do dinossauro, famoso no Chrome por nos entreter quando estamos sem conexão. Nele iremos utilizar HTML, CSS e JavaScript, abordando de maneira simples diversos conceitos introdutórios importantes para programação na web como tags básicas de HTML, manipulação de eventos, funções e manipulação de elementos HTML usando JavaScript, estilização e animações básicas com CSS.
 
---
+++ projeto_jogo_dino/script.js
@@ -0,0 +1,73 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false; // Diz se o dino está pulando.
+
+let position = 0; //Posição inicial do dino.
+
+//Função para verificar se o usuário pressionou a barra de espaço para o dino pular
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {  //Se a tecla pressionada foi barra de espaço(keycode 32), então aciona função jump()
+        if (!isJumping) {
+            jump();
+        }       
+    }
+}
+
+//Função para o pulo do dino
+function jump() {
+
+    isJumping = true;  // dino está pulando.
+
+    let upInterval = setInterval(() => {  // setInterval irá repetir o bloco de código a cada intervalo determinado, no caso 20ms.
+        if (position >= 150) {  //Se a posição do dino for maior ou igual a 150px então o upInterval será limpo, ou seja o dino pára.
+            clearInterval(upInterval);
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) { //Se a posição for menor ou igual a 0 o dino pána.
+                    clearInterval(downInterval);
+                    isJumping = false; // se chegou na posição 0, dino n está mais pulando e pode pular novamente.
+                }else {   // Caso a posição seja maior que 0 o dino desce 20px a cada 20ms.     
+                    position -= 20;
+                    dino.style.bottom = position + 'px'; //Passando parâmetro de css para o dino.
+                }
+            }, 20);
+        }else { // Caso a posição do dino não tenha atingido 150px, ele subirá 20px a cada 20ms.
+            //Subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+//Função para criar os cactus
+function createCactus() {
+    const cactus = document.createElement('div'); //Aqui o js vai gerar elementos html na constante cactus, no caso uma div.
+    let cactusPosition = 1000; //Define posição do cactus.
+    let randomTime = Math.random() * 6000; //Criará números aleatórios de cactus na tela.
+
+    cactus.classList.add('cactus'); //Add uma classe de nome cactus na div dentro da constante cactus.
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus); //Add um filho com appendChild.
+
+    let leftInterval = setInterval(() => { // Movimentará o cactus para a esquerda na tela.
+
+        if (cactusPosition < -60) {    //Se a posição do cactus for menor que -60 que é a sua largura ele irá desaparecer.
+            clearInterval(leftInterval);
+            background.removeChild(cactus); // Vai remover o filho cactus do background, para sumir.
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo!</h1>';
+        
+        } else { // Caso o cactus não tenha tingido a posição -60 segue movendo -10px a cada 20ms.
+            cactusPosition -= 10;  //Velocidade que se moverá para a esquerda.
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime)  // setTimeout executa uma determinada função depois de um determinado tempo, no caso cria os cactus de forma randômica.
+}
+
+createCactus();
+document.addEventListener('keydown', handleKeyUp);   //keydown vai verificar se o usuário apertou uma tecla.
